42cb2abbcfea75da7bb8f33b7f4e5251558ce393
vineshsingh90 <vineshsingh90@gmail.com>
2021-12-31 22:03:52 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'ckeditor5-with-angular';
+  public Editor = ClassicEditor;
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, CKEditorModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
